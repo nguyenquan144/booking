59785b440804cc8172d56f641e550e8d238df8cf
Quan Nguyen <nguyenducquanptvn@gmail.com>
2023-05-19 04:57:05 +0700

    first commit

---
+++ NODEJS/src/seeders/seeder-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'quan',
+      lastName: 'david',
+      address: 'USA',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
